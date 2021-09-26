9c603a01d98542631d6376d16e3c4272c10ebf9f
rmonsterlet <romain.monsterlet@gmail.com>
2021-09-27 00:37:49 +0200

    git commit -m "first commit"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, NgZone, Input } from '@angular/core';
+import { Shape } from './shape';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Swamp';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>;
+  ctx: CanvasRenderingContext2D;
+  requestId;
+  interval;
+  shapes: Shape[] = [];
+  value = 10;
+
+  constructor(private ngZone: NgZone) {}
+
+  ngAfterViewInit() {
+    this.ctx = this.canvas?.nativeElement.getContext('2d');
+    this.ngZone.runOutsideAngular(() => this.tick());
+    this.setIntervals();
+  }
+
+  setIntervals() {
+    setInterval(() => {
+      this.tick();
+    }, 400);
+    setInterval(() => {
+      this.clear();
+    }, 30000);
+  }
+
+  tick() {
+    const shape = new Shape(this.ctx);
+    this.shapes = this.shapes.concat(shape);
+    this.shapes.forEach((shape: Shape) => {
+      shape.spawnCircle(this.value);
+    });
+    this.requestId = requestAnimationFrame(() => this.tick);
+  }
+
+  clear() {
+    this.shapes= [];
+    this.ctx?.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+  }
+
+  destroy() {
+    clearInterval(this.interval);
+    cancelAnimationFrame(this.requestId);
+  }
+
+  ngOnDestroy() {
+    this.destroy();
+  }
 }
---
+++ src/app/shape.ts
@@ -0,0 +1,27 @@
+export class Shape {
+    private color = this.getRandomColor();
+    constructor(private ctx: CanvasRenderingContext2D) {}
+  
+    spawnRect() {
+      const wh = Math.random()*250 + 5;
+      this.ctx.fillStyle = this.color;
+      this.ctx.fillRect(Math.random() * this.ctx.canvas.width - wh, Math.random() * this.ctx.canvas.height, wh, wh);
+    }
+
+    spawnCircle(shapeValue) {
+      const wh = Math.random()*250 + 5;
+      this.ctx.beginPath();
+      this.ctx.fillStyle= this.color;
+      this.ctx.arc(Math.random() * this.ctx.canvas.width - wh, Math.random() * this.ctx.canvas.height, Math.random() * shapeValue, 0, 2 * Math.PI);
+      this.ctx.fill();
+    }
+
+    getRandomColor() {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+  }
